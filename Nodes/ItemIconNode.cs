8aa3d150a54e0e1d0062f3be709b51ab52db9e84
AtmoOmen <atmoomen2004@outlook.com>
2026-09-26 18:31:48 +0800

    ItemIconNode

---
+++ Nodes/ItemIconNode.cs
@@ -0,0 +1,69 @@
+﻿using Dalamud.Game.Addon.Events;
+using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using KamiToolKit.Nodes;
+using Lumina.Excel.Sheets;
+using OmenTools.Extensions;
+using OmenTools.Interop.Game.Lumina;
+
+namespace OmenTools.KamiToolKit.Nodes;
+
+public class ItemIconNode : IconNode
+{
+    /// <summary>
+    ///     Gets or sets the displayed item.
+    /// </summary>
+    public uint ItemID
+    {
+        get;
+        set
+        {
+            if (field == value) 
+                return;
+            if (!LuminaGetter.TryGetRow(value, out Item row))
+                return;
+            
+            field       = value;
+            IconId      = row.Icon;
+            ItemTooltip = row.RowId;
+        }
+    }
+    
+    /// <summary>
+    ///     Action that is triggered when the item is being clicked.
+    /// </summary>
+    public AtkEventListener.Delegates.ReceiveEvent? OnClick { get; set; }
+
+    public unsafe ItemIconNode()
+    {
+        AddNodeFlags(NodeFlags.HasCollision);
+        
+        AddEvent
+        (
+            AtkEventType.MouseOver,
+            () => IAddonEventManager.Instance().SetCursor(AddonCursorType.Clickable)
+        );
+        
+        AddEvent
+        (
+            AtkEventType.MouseOut,
+            () => IAddonEventManager.Instance().ResetCursor()
+        );
+            
+        AddEvent
+        (
+            AtkEventType.MouseClick,
+            MouseClickHandler
+        );
+    }
+
+    private unsafe void MouseClickHandler
+    (
+        AtkEventListener* thisPtr,
+        AtkEventType      eventType,
+        int               eventParam,
+        AtkEvent*         atkEvent,
+        AtkEventData*     atkEventData
+    )
+        => OnClick?.Invoke(thisPtr, eventType, eventParam, atkEvent, atkEventData);
+}
